8e404a3e17b60b499f83937f3e4b0c7deeb78f52
Syudagye <44324659+Syudagye@users.noreply.github.com>
2025-04-30 08:03:44 +0200

    Updated flake inputs (#42)

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
-      url = "github:nix-community/fenix";
+      url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     naersk = {
